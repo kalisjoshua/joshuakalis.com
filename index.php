10590e86f1c684979d9b1e7138b0aa1b2e44ff91
Joshua T Kalis <kalisjoshua@gmail.com>
2010-06-16 16:32:08 -0400

    removed some variables

--- index.php
+++ index.php
@@ -1,14 +1,9 @@
 <?php
 
-$template = "default";
+$theme = "default";
 
-$siteURL = ($_SERVER['HTTP_HOST'] != "localhost")? "http://joshuakalis.com": "http://localhost";
-$sources = "$siteURL/website";
-$resources = "$sources/themes/$template";
-$portfolioFiles = "$sources/portfolio.files";
-
-$siteTitle = "joshua kalis - designer, programmer";
-$siteDesc = "";
+$pageTitle = "joshua kalis - designer, programmer";
+$pageDesc = "";
 $siteMenu = Array(
 	 "about"
 	,"portfolio"
@@ -17,13 +12,12 @@ $siteMenu = Array(
 //	,"projects"
 );
 
+$domain = ($_SERVER['HTTP_HOST'] != "localhost")? "http://joshuakalis.com": "http://localhost";
+
 $API_request = strtolower(preg_replace("/\?.*/", "", $_SERVER['REQUEST_URI']));
 $API_request = preg_match('/^\/?$/', $API_request)? Array("about"): explode("/", substr($API_request, 1));
-if($API_request[0] == "error") {
-    array_shift($API_request);
-}
 
 $page = $API_request[0];
 
 $document = "website/".$page.".php";
-require "website/themes/$template/template.php";
+require "website/themes/$theme/template.php";
