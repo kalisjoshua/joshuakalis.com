fba9600b16061c225ddb5d409b055c6c0e5ab6c1
Joshua T Kalis <kalisjoshua@gmail.com>
2010-06-16 16:33:23 -0400

    removed some variables

--- website/themes/default/template.php
+++ website/themes/default/template.php
@@ -2,25 +2,25 @@
 <html>
 <head>
 <meta charset="utf-8" />
-<title><?php echo ucfirst($page)." - $siteTitle"; ?></title>
-<meta name="description" content="<?php echo $siteDesc;?>" />
+<title><?php echo ucfirst($page)." - $pageTitle"; ?></title>
+<meta name="description" content="<?php echo $pageDesc;?>" />
 
-<link rel="stylesheet" href="<?php echo $resources; ?>/css/css.min.php" type="text/css" />
-<link rel="stylesheet" href="<?php echo $resources; ?>/css/print.css" type="text/css" media="print" />
+<link rel="stylesheet" href="<?php echo "$domain/website/themes/$theme"; ?>/css/css.min.php" type="text/css" />
+<link rel="stylesheet" href="<?php echo "$domain/website/themes/$theme"; ?>/css/print.css" type="text/css" media="print" />
 </head>
 
 <body class="<?php echo ucfirst($page); ?>">
     <p id="SkipToContent"><a href="#content">Skip to main content</a></p>
 
     <header role="banner">
-        <a href="<?php echo $siteURL;?>"><?php echo $siteTitle; ?></a>
-        <h1><?php echo ucfirst($page)." - $siteTitle"; ?></h1>
+        <a href="<?php echo $domain;?>"><?php echo $pageTitle; ?></a>
+        <h1><?php echo ucfirst($page)." - $pageTitle"; ?></h1>
     </header>
     
     <nav role="navigation">
         <ul>
 <?php foreach( $siteMenu as $item ) { ?>
-            <li<?php if( strcasecmp($page, $item) == 0 ) { echo " class=\"active\""; } ?>><a href="<?php echo "$siteURL/$item"; ?>"><?php echo $item; ?></a></li>
+            <li<?php if( strcasecmp($page, $item) == 0 ) { echo " class=\"active\""; } ?>><a href="<?php echo "$domain/$item"; ?>"><?php echo $item; ?></a></li>
 <?php } ?>
         </ul>
     </nav>
@@ -32,12 +32,12 @@
     </section>
  	
     <footer role="contentinfo">
-        <a href="http://validator.w3.org/check?uri=<?php echo "$siteURL/$page"; ?>" title="run HTML validator">HTML 5</a> :
+        <a href="http://validator.w3.org/check?uri=<?php echo "$domain/$page"; ?>" title="run HTML validator">HTML 5</a> :
         <a href="http://jigsaw.w3.org/css-validator/check/referer" title="run CSS validator">css 3.0</a> :
         <a href="http://www.w3.org/WAI/WCAG2AAA-Conformance" title="Explanation of WCAG 2.0 Level Triple-A Conformance">W3C WAI-AAA WCAG 2.0</a>
     </footer>
     
 <!--[if IE]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
-<script src="<?php echo $resources; ?>/js/js.min.php"></script>
+<script src="<?php echo "$domain/website/themes/$theme"; ?>/js/js.min.php"></script>
 </body>
 </html>
