04705a5e772684216a9132438ef9993adfe3fbcf
Joshua T Kalis <kalisjoshua@gmail.com>
2010-06-17 15:24:49 -0400

    refactored some lagic into easier bits

--- index.php
+++ index.php
@@ -14,10 +14,10 @@ $siteMenu = Array(
 
 $domain = ($_SERVER['HTTP_HOST'] != "localhost")? "http://joshuakalis.com": "http://localhost";
 
-$page = strtolower(preg_replace("/\?.*/", "", $_SERVER['REQUEST_URI']));
-$page = preg_match('/^\/?$/', $page)? Array("about"): explode("/", substr($page, 1));
-
-$page = $page[0];
+$page = strtolower(preg_replace("/^\/|\?.*/", "", $_SERVER['REQUEST_URI']));
+if ($page == "") {
+    $page = "about";
+}
 
 $document = "website/".$page.".php";
 require "website/themes/$theme/template.php";
