b71e9306635e684e931a1b6b79c2a2ea68c4ec72
Joshua T Kalis <kalisjoshua@gmail.com>
2010-10-25 07:11:05 -0400

    changes from last time I edited; this is totally a useless comment

--- website/about.php
+++ website/about.php
@@ -1,8 +1,14 @@
 
-<p>The web is where I work; I love developing on a platform that is so available and wonderfully dynamic. HTML5 and JavaScript are going to change, in a big way, how we interact with information on the web. For a long time we have been held back by limitations inherent in archaic technologies and terrible formats (i.e. Flash).</p>
-
 <p>I enjoy creating. This has been the common thread throughout all of my working history. I've been a: carpenter, cook, designer, developer, landscaper, machinist, programmer, and student (in alphabetic order).</p>
 
 <p>My primary passion right now is JavaScript and User Interfaces. Early on I found a love for forms markup and processing; in particular it was the interaction of user and the application. The various form elements were, at times, limiting but in the end had to do the job and now with JavaScript the options are becoming more varied. Data-entry can now be extremely responsive, and even fun.</p>
 
-<p>Even beyond data-entry, the display and organization of data is evolving with new technologies and techniques. Dynamic data, graphs with layers of information, mash-ups between disconnected data-sets; these are enabling quicker analysis of data that used to be tied to a single system and are now open on the web to everyone in as many ways as can be imagined.</p>
+<p>Even beyond data-entry, the display, organization, and processing of data is evolving with new technologies and techniques. Dynamic data, graphs with layers of information, mash-ups between disconnected data-sets; these are enabling quicker analysis of data that used to be tied to a single system and are now open on the web to everyone in as many ways as can be imagined.</p>
+
+<p>I have been experimenting lately with HTML5 and jQuery; obviously used on this site and worth the time. I love learning new things so I have been starting to read about functional programming in a few languages: Clojure, OCaml, and Scheme; but have not yet begun working with them.</p>
+<!-->
+web standards
+accessible applications
+useable design
+browser features (Safari's "reader")
+<-->
