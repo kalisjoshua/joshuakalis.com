f3ee976011d46061d329fd3fdbc6351290e4c488
Joshua T Kalis <kalisjoshua@gmail.com>
2010-11-01 09:56:32 -0400

    added a new accent2 color

--- framework/themes/default/css/_variables.php
+++ framework/themes/default/css/_variables.php
@@ -4,7 +4,7 @@ $theme = json_decode(file_get_contents("../../../models/site.config.json"))->the
 $schemes = Array(
 	//		pageBG		contentBG	contentFG	menuBG1		menuFG1		menuBG2		menuFG2		accent1		accent2
 	//		0			1			2			3			4			5			6			7			8
-	 Array( "FFFFFF",	"FFFFFF",	"1D1E1F",	"09628D",	"666666",	"FFFFFF",	"FFFFFF",	"4E90AD",	"FFFFFF" )
+	 Array( "FFFFFF",	"FFFFFF",	"1D1E1F",	"09628D",	"666666",	"FFFFFF",	"FFFFFF",	"4E90AD",	"D2DDD5" )
 	,Array( "000000",	"000000",	"EADEDD",	"541B15",	"BBBBBB",	"000000",	"000000",	"902D24",	"000000" )
 	,Array( "211603",	"FFFFFF",	"FFFFFF",	"6FA2BC",	"666666",	"211603",	"FFFFFF",	"4E90AD",	"211603" )
 );
