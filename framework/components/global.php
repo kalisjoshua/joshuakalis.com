c4d7e37f23e36bb22726263fd6d5f2562ddd8480
Joshua T Kalis <kalisjoshua@gmail.com>
2010-11-23 14:01:04 -0500

    moved functionality from build_menu.php into global.php for simplicity

--- framework/components/global.php
+++ framework/components/global.php
@@ -3,3 +3,20 @@
 function load_json ($file) {
     return json_decode((is_file($file) ? file_get_contents($file) : "{}"));
 }
+
+// build menu structure
+// **consider reading in meta information from each page for links of main menu
+function build_menu($request_uri) {
+    $menu = glob("../views/*.tmpl");
+
+    array_walk(
+        $menu,
+        function (&$item, $key, $depth) {
+            $page = preg_replace("/..\/views\/(.*)\.\w+$/", "$1", $item);
+            $item = json_decode("{\"link\": \"$page\", \"url\": \"$depth$page\"}");
+        },
+        str_pad("", (substr_count($request_uri, "/") - 1) * 3, "../")
+    );
+    
+    return $menu;
+}
