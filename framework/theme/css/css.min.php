a1ecd9cd670f33c88f092cb66004d4a94b90772d
Joshua T Kalis <kalisjoshua@gmail.com>
2011-02-03 09:32:13 -0500

    css working; without color scheme preview page

--- framework/theme/css/css.min.php
+++ framework/theme/css/css.min.php
@@ -1,19 +1,33 @@
 <?php
 header('Content-type: text/css');
-$isIE = preg_match( "/MSIE/", $_SERVER['HTTP_USER_AGENT'] );
-ob_start("compress");
-function compress($buffer) {
-    return preg_replace(array('/\s*([,\{\}:;])\s*/', '/[\n\r\t]+/', '!/\*[^*]*\*+([^/][^*]*\*+)*/!'), array('\1', ''), $buffer);
-}
+$days_to_cache = 30;
+header('Expires: '.gmdate('D, d M Y H:i:s',time() + (60 * 60 * 24 * $days_to_cache)).' GMT');
+
+$prefs = json_decode(file_get_contents("../../models/site.config.json"));
+$ini = parse_ini_file("variables.ini", true);
+$ini = $ini[$prefs->scheme];
+
+$names = array_map(function ($v) { return "_$v"; }, array_keys($ini));
+$colors = array_map(function ($v) { return "$v"; }, array_values($ini));
+
+$replace = array('/\s*([,\{\}:;])\s*/', '/[\n\r\t]+/', '!/\*[^*]*\*+([^/][^*]*\*+)*/!');
+$min = array('\1', '');
+/* */
+ob_start(function ($buffer) {
+        global $colors, $names, $replace, $min;
+        $buffer = str_replace($names, $colors, $buffer);
+        $buffer = preg_replace($replace, $min, $buffer);
+        return $buffer;
+    });
 
 include("reset.css");
 include("960.css");
 
-include("content.css.php");
-include("layout.css.php");
-include("regex.css.php");
+include("layout.css");
+include("content.css");
 
+include("regex.css");
 include("lightbox0.5.css");
 
 ob_end_flush();
-?>
+/* */
