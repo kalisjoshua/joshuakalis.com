24da6fc28cb023f308ee65af2d2c3d94d20817ac
Joshua T Kalis <jkalis@centurionmp.com>
2011-02-25 11:29:29 -0500

    changed from _ to #

--- framework/theme/css/css.min.php
+++ framework/theme/css/css.min.php
@@ -7,7 +7,7 @@ $prefs = json_decode(file_get_contents("../../models/site.config.json"));
 $ini = parse_ini_file("variables.ini", true);
 $ini = $ini[$prefs->scheme];
 
-function map_names($v) { return "_$v"; }
+function map_names($v) { return "#$v"; }
 $names = array_map("map_names", array_keys($ini));
 function map_colors($v) { return "$v"; }
 $colors = array_map("map_colors", array_values($ini));
