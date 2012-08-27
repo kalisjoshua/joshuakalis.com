2cdcc650d0e38a40f2c0c2119eed07be48f54c18
Joshua T Kalis <kalisjoshua@gmail.com>
2012-08-27 15:42:15 -0400

    updated regex util

--- framework/theme/js/regex_form.js
+++ framework/theme/js/regex_form.js
@@ -1,34 +1,64 @@
+/*jshint laxcomma:true*/
+/*global jQuery*/
 (function ($) {
-    $(function () {	
-        (function (regexEval) {
-        	// the form should never submit
-        	$(".RegExp form#regex").
-            	submit(function (event) {
-            		event.preventDefault();
-            	});
-        	// add event handlers to the regex form fields
-        	$(".RegExp form#regex #haystack, .RegExp form#regex input[type=checkbox]").change(regexEval);
-        	$(".RegExp form#regex #pattern").keyup(regexEval);
-        }(function (event) {
-    		try {
-    			var haystack = $("#haystack").val(),
-    				pattern = new RegExp($("#pattern").val(), 
-    					($("#global")[0].checked ? "g" : "") +
-    					($("#insensitive")[0].checked ? "i" : "") +
-    					($("#multiline")[0].checked ? "m" : "")),
-    				matches = pattern.exec(haystack);
-		    
-    			if (haystack && pattern) {
-    				$(matches).each(function(indx, ele) {
-    					haystack = haystack.replace(RegExp("(" + ele + ")", "g"), "<span>$1</span>");
-    				});
-    				$("#results").html("<pre>" + haystack + "</pre>");
-    			}
-    		}
-    		catch (err) {
-    			// attempting to create a new RegExp Object with an incomplete pattern
-    			// will throw and error but we don't need to handle that error here.
-    		}
-    	}));
+    "use strict";
+
+    function entt (str, is_r) {
+        return str
+            .replace(/</g, (is_r ? "(?:&lt;)" : "&lt;"))
+            .replace(/>/g, (is_r ? "(?:&gt;)" : "&gt;"));
+    }
+
+    function pre (str) {
+        return "<pre>%1</pre>"
+            .replace("%1", str);
+    }
+
+    // return a valid regular expression or false
+    function regex (temp) {
+        try {
+            temp = RegExp.apply(null, entt(temp, true).match(rREGEX).slice(1));
+            return (rBLANK !== temp.toString() ? temp : false);
+        } catch (err) {
+            return false;
+        }
+    }
+
+    function wrap (str, regex, sub) {
+        return entt(str)
+            .replace(regex, sub || "<span>$&</span>") || str;
+    }
+
+    var rBLANK = (new RegExp()).toString()
+      , rREGEX = /^\/(.*)\/([gim]{0,3})$/;
+
+    $.fn.ready(function () {
+        $("fieldset").hide();
+
+        var haystack  = $("#haystack")
+          , pattern   = $("#pattern")
+          , replace   = $("#replace")
+          , results   = $("#results")
+          , run       = function (event) {
+                event && event.preventDefault();
+
+                var temp = regex(pattern.val());
+
+                // highlight the text only if the current pattern is valid
+                results.html(pre(temp
+                    ? wrap(haystack.val(), temp, replace.val())
+                    : entt(haystack.val())));
+            };
+
+        // the form should never actually submit
+        $("#regex")
+            .on("submit", run);
+
+        haystack
+            .on("change", run);
+
+        pattern
+            .add(replace)
+            .on("keyup", run);
     });
 }(jQuery));
